498f4cf84a19f6b5a3b91bb4513dd083a841e4ed
Krisin9527 <119922532+Krisin9527@users.noreply.github.com>
2023-03-03 13:55:29 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,77 @@
+var el = document.getElementById("footer");
+el.innerHTML = `<div class="container text-center text-md-start mt-5">
+       
+<div class="row mt-3">
+
+  <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+    <h6 class="text-uppercase fw-bold mb-4">
+      <img src="./img/LOGO.png" alt="LOGO" style="width: 32px;height: 32px;">
+    </h6>
+    <a href="https://www.facebook.com/" class="me-4 text-reset ">
+      <i class="fab fa-facebook-f"></i>
+    </a>
+    <a href="https://www.instagram.com/" class="me-4 text-reset">
+      <i class="fab fa-instagram"></i>
+    </a>
+    <a href="https://discord.com/" class="me-4 text-reset ">
+      <i class="fab fa-discord"></i>
+    </a>
+  </div>
+
+  <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+    <!-- Links -->
+    <h6 class="text-uppercase fw-bold mb-4">
+      EXPERT
+    </h6>
+    <p>
+      <a href="./expert.html" class="text-reset">達人頁面</a>
+    </p>
+    <p>
+      <a href="#exampleModal" class="text-reset" data-mdb-toggle="modal" data-mdb-target="#exampleModal">成為達人</a>
+    </p>
+    <p>
+      <a href="#beAexpert" class="text-reset" data-mdb-toggle="modal" data-mdb-target="#beAexpert">註冊流程</a>
+    </p>
+
+  </div>
+  <!-- Grid column -->
+
+  <!-- Grid column -->
+  <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+    <!-- Links -->
+    <h6 class="text-uppercase fw-bold mb-4">
+      CASE
+    </h6>
+    <p>
+      <a href="./case.html" class="text-reset">發案頁面</a>
+    </p>
+    <p>
+      <a href="#exampleModal2" class="text-reset" data-mdb-toggle="modal" data-mdb-target="#exampleModal2">我要發案</a>
+    </p>
+    <p>
+      <a href="#!" class="text-reset"  data-mdb-toggle="modal" data-mdb-target="#doCase">發案步驟</a>
+    </p>
+  </div>
+
+  <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+    <!-- Links -->
+    <h6 class="text-uppercase fw-bold mb-4">CONTACT</h6>
+
+    <p>
+      <i class="fas fa-envelope me-3"></i>
+      nhsh11206@gmail.com
+    </p>
+    <p><i class="fas fa-phone me-3"></i>+886967324077</p>
+  </div>
+
+</div>
+
+</div>
+</section>
+
+<div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+© 2023 全人 All rights reserved.
+</div>`
+
+         
